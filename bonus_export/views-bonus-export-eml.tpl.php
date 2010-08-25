f31b32ced9e84b60e27b540bee047e29018de7ad
annaship <ashipunova3@gmail.com>
2010-08-25 18:42:05 -0400

    work with fields

--- bonus_export/views-bonus-export-eml.tpl.php
+++ bonus_export/views-bonus-export-eml.tpl.php
@@ -4,7 +4,8 @@
  * Template to display a view as an eml.
  * TODO: 
  * 1) add all tags
- * 2) 
+ * 2) parameters in tag <geographicCoverage id="GEO-13"> 
+ *    as print '<'.$label.' '.$id_name.'="'.$id_value.'">'.$content.'</'.$label.'>';
  *
  */
 
@@ -19,6 +20,67 @@ function print_open_tag($tag) {
 function print_close_tag($tag) {
   print '</'.$tag.'>';
 }
+
+function print_all_fields($field_arr, $node) {
+  // dpr($node);
+  foreach ($field_arr as &$value) {
+    $label = $value;
+    print_open_tag($label);
+    $a = $node->$value;
+    foreach ($a as $key1 => $value1){
+      foreach ($value1 as $key2 => $value2){
+        print_tag_line($key2, $value2);                 
+      }
+    }
+    print_close_tag($label);
+  }
+}
+
+$dataset_arr = array("nid", 
+  "field_abstract", 
+  "field_beg_end_date", 
+  "field_dataset_add_info", 
+  "field_dataset_assct_biblio", 
+  "field_dataset_contact", 
+  "field_dataset_ext_assoc", 
+  "field_dataset_id", 
+  "field_dataset_instrument", 
+  "field_dataset_maintenance", 
+  "field_dataset_methods", 
+  "field_dataset_owner", 
+  "field_dataset_purpose", 
+  "field_dataset_quality", 
+  "field_dataset_sevid", 
+  "field_publication_date", 
+  "field_short_name"
+  // "field_dataset_datafile_ref", 
+  // "field_dataset_datamanager_ref", 
+  // "field_dataset_fieldcrew_ref", 
+  // "field_dataset_labcrew_ref", 
+  // "field_dataset_site_ref", 
+  // "field_var_ref"
+);
+
+
+$datafile_arr = array("nid",
+  "vid",
+  "field_datafile_date", 
+  "field_datafile_description", 
+  "field_datafile_name", 
+  // "field_datafile_site_ref", 
+  // "field_datafile_variable_ref", 
+  "field_data_file", 
+  "field_delimiter", 
+  "field_method_description", 
+  "field_num_footer_lines", 
+  "field_num_header_line", 
+  "field_orientation", 
+  "field_quality_control", 
+  "field_quote_character", 
+  "field_record_delimiter", 
+  "field_sampling_description"
+  ); 
+
  
 print '<?xml version="1.0" encoding="UTF-8" ?>';
 ?>
@@ -27,90 +89,22 @@ print '<?xml version="1.0" encoding="UTF-8" ?>';
 
 <?php foreach ($themed_rows as $count => $row): 
 /* Dataset
-field_abstract
-field_beg_end_date
-field_dataset_add_info
-field_dataset_assct_biblio
-field_dataset_contact
-field_dataset_datafile_ref
-field_dataset_datamanager_ref
-field_dataset_ext_assoc
-field_dataset_fieldcrew_ref
-field_dataset_id
-field_dataset_instrument
-field_dataset_labcrew_ref
-field_dataset_maintenance
-field_dataset_methods
-field_dataset_owner
-field_dataset_purpose
-field_dataset_quality
-field_dataset_sevid
-field_dataset_site_ref
-field_publication_date
-field_short_name
-field_var_ref
 */
       print_open_tag("Dataset");
         foreach ($row as $field => $content):          
-          // $label = "DatasetId";
-          // print_tag_line($label, $content); 
           $node = node_load($content);
-          $field_arr = array("nid", 
-          "field_abstract", 
-          "field_beg_end_date", 
-          "field_dataset_add_info", 
-          "field_dataset_assct_biblio", 
-          "field_dataset_contact", 
-          "field_dataset_ext_assoc", 
-          "field_dataset_id", 
-          "field_dataset_instrument", 
-          "field_dataset_maintenance", 
-          "field_dataset_methods", 
-          "field_dataset_owner", 
-          "field_dataset_purpose", 
-          "field_dataset_quality", 
-          "field_dataset_sevid", 
-          "field_publication_date", 
-          "field_short_name");
-          foreach ($field_arr as &$value) {
-            $label = $value;
-            print_open_tag($label);
-            $a = $node->$value;
-            // print($value);
-            // dpr($a);
-            // dpr($node->field_dataset_id[0][value]);
-            // $label = "DatasetId";
-            // print_tag_line($label, $node->$value); 
-            // $a = $node->field_beg_end_date;
-            foreach ($a as $key1 => $value1){
-              foreach ($value1 as $key2 => $value2){
-                // echo "\$value1[$key2] => $value2.\n";
-                print_tag_line($key2, $value2); 
-                // print $key2."->".$value2.", ";
-                // foreach ($a as $k => $v) {
-                    // echo "\$a[$k] => $v.\n";
-                // }
-                // print_tag_line($m, $node->); 
-                // dpr($node->field_dataset_id[$i][$m]);
-                // dpr($a);
-              }
-            }
-            print_close_tag($label);
-          }
-          
-          
-          
-          
+          print_all_fields($dataset_arr, $node);
           if($node->type == "data_set"):
-            $datafiles = $node->field_dataset_datafile_ref;   
+            $datafiles_ref = $node->field_dataset_datafile_ref;  
             print_open_tag("datafiles");
-            foreach ($datafiles as &$datafile):   
+            foreach ($datafiles_ref as &$datafile):   
               print_open_tag("datafile");
               $datafile_nid = $datafile[nid];      
-              $dd    = node_load($datafile_nid);
-              $vars  = $dd->field_datafile_variable_ref;     
+              $datafiles    = node_load($datafile_nid);
+              print_all_fields($datafile_arr, $datafiles);
+              $vars  = $datafiles->field_datafile_variable_ref;     
               $label = "DatafileNid";
-              print_tag_line($label, $dd->nid); 
+              print_tag_line($label, $datafiles->nid); 
               print_open_tag("variables");              
               foreach ($vars as &$var):    
                 print_open_tag("variable");              
@@ -124,10 +118,10 @@ field_var_ref
                 print_tag_line($label, $maximum); 
                 print_close_tag("variable");              
               endforeach; //($vars as &$var)
-              unset($dd);
+              unset($datafiles);
               print_close_tag("variables");              
               print_close_tag("datafile");              
-            endforeach; //($datafiles as &$datafile)
+            endforeach; //($datafiles_ref as &$datafile)
             print_close_tag("datafiles");              
           endif; //($type == "data_set")
         endforeach; //($row as $field => $content)
